9cee28ddc1f135144578beddb9eb697f4b1dad6e
Jeremy Locatelli <jlocatelli@csumb.edu>
2017-09-08 20:40:21 +0000

    done for the day

---
+++ challenges/CardGame/card.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> hell</title>
+    </head>
+    <body>
+    <h1> hell </h1>
+    <?php
+        
+        function getSuit()
+        {
+        
+    
+    
+    
+    ?>
+    
+    
+    
+    
+    
+    </body>
+</html>
--- labs/lab2/index.php
+++ labs/lab2/index.php
@@ -6,54 +6,8 @@
     <body>
         
         <?php
-        
-        $randomValue1 = rand(0,2);
-        $randomValue2 = rand(0,2);
-        $randomValue3 = rand(0,2);
-        function displaySymbol($randomValue)
-        {
-            
-            switch ($randomValue)
-            {
-             case 0: $symbol = "seven";
-                    break;
-             case 1: $symbol = "lemon";
-                    break;
-             case 2: $symbol = "orange";
-                    break;
-            }
-            echo "<img src='img/$symbol.png' alt='$symbol' title=\"$symbol\" width=\"70\" />";
-        }
-        displaySymbol($randomValue1);
-        displaySymbol($randomValue2);
-        displaySymbol($randomValue3);
-        
-        function displayPoints()
-        {
-            //find out which of the three values are the same
-            if($randomValue1 == 0 && $randomValue1 == $randomValue2 && $randomValue1 == $randomValue3)
-            {
-                echo "Wow 1000 Points";
-            }
-            if($randomValue1 == 1 && $randomValue1 == $randomValue2 && $randomValue1 == $randomValue3)
-            {
-                echo "Wow 500 points";
-            }
-            if($randomValue1 == 2 && $randomValue1 == $randomValue2 && $randomValue1 == $randomValue3)
-            {
-                echo "Wow 250 points";
-            }
-        }
-        
-        displayPoints();
-        
-        
-        
-      
-        
-        
-        
-        
+        include 'inc/functions.php';
+        play();
         ?>
         
         
