c6f8d3b97e8239a75783ac83ac892d0714cff021
Jeremy Locatelli <jlocatelli@csumb.edu>
2017-09-11 00:49:44 +0000

    yeah

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
         <title> </title>
     </head>
     <body>
-asdfasdf
+     <a href="Homework_1/index.php">Homework 1</a>
+     <a href="labs/lab2/index.php">Slot Machine</a>
+     
     </body>
 </html>
