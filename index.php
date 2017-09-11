56116500842e0fbb9eef4996b879fbafa50b2a0b
Jeremy Locatelli <jlocatelli@csumb.edu>
2017-09-11 21:12:58 +0000

    update main index

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     </head>
     <body>
      <a href="Homework_1/index.php">Homework 1</a>
-     <a href="labs/lab2/index.php">Slot Machine</a>
-     
+     <a href="labs/lab2/index.php">Lab 2 Slot Machine</a>
+     <a href="labs/lab1/index.php">Lab 1 Personal Portfolio</a>
     </body>
 </html>
