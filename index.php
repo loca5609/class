fb4d83cf208ca4c5f72f36d2c64607a0808c1e6b
Jeremy Locatelli <jlocatelli@csumb.edu>
2017-09-12 21:06:28 +0000

    Added homework folder rather than having all homeworks in a seperate folder in root

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         <title> </title>
     </head>
     <body>
-     <a href="Homework_1/index.php">Homework 1</a>
+     <a href="homework/homework_1/index.php">Homework 1</a>
      <a href="labs/lab2/index.php">Lab 2 Slot Machine</a>
      <a href="labs/lab1/index.php">Lab 1 Personal Portfolio</a>
     </body>
