9ccd9afe3e9265073eb4ea21bef1112feac50325
Jeremy Locatelli <jlocatelli@csumb.edu>
2017-09-19 22:26:10 +0000

    added homework 2 link to main index

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     </head>
     <body>
      <a href="homework/homework_1/index.php">Homework 1</a>
+     <a href="homework/homework_2/index.php">Homework 2</a>
      <a href="labs/lab2/index.php">Lab 2 Slot Machine</a>
      <a href="labs/lab1/index.php">Lab 1 Personal Portfolio</a>
     </body>
