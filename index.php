3271e9d9eed036d4b1823c3d7d5cf7a339cb63e3
Jeremy Locatelli <jlocatelli@csumb.edu>
2017-09-20 04:52:31 +0000

    spacing in homework 2 index.php

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     </head>
     <body>
         <header><h1>CST-338 Work</h1></header>
-     <a href="homework/homework_1/index.php">Homework 1</a> <br/>
-     <a href="homework/homework_2/index.php">Homework 2</a> <br/>
+     <a href="homework/homework_1/index.php">Homework 1: Arduino Tutorial</a> <br/>
+     <a href="homework/homework_2/index.php">Homework 2: Dice Roller</a> <br/>
      <a href="labs/lab2/index.php">Lab 2 Slot Machine</a> <br/>
      <a href="labs/lab1/index.php">Lab 1 Personal Portfolio</a> <br/>
     </body>
