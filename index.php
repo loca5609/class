2c978d7020647b181581c9cf64d0e0dd79bf7c1c
Jeremy Locatelli <jlocatelli@csumb.edu>
2017-09-20 00:26:53 +0000

    updated root index.php

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title> </title>
+        <title>CST-338</title>
     </head>
     <body>
-     <a href="homework/homework_1/index.php">Homework 1</a>
-     <a href="homework/homework_2/index.php">Homework 2</a>
-     <a href="labs/lab2/index.php">Lab 2 Slot Machine</a>
-     <a href="labs/lab1/index.php">Lab 1 Personal Portfolio</a>
+        <header><h1>CST-338 Work</h1></header>
+     <a href="homework/homework_1/index.php">Homework 1</a> <br/>
+     <a href="homework/homework_2/index.php">Homework 2</a> <br/>
+     <a href="labs/lab2/index.php">Lab 2 Slot Machine</a> <br/>
+     <a href="labs/lab1/index.php">Lab 1 Personal Portfolio</a> <br/>
     </body>
 </html>
