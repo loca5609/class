657390f10ef65bddeec3a92177064c47eea1acdd
Jeremy Locatelli <jlocatelli@csumb.edu>
2017-09-25 03:19:07 +0000

    updated root index.php i always forget this

--- index.php
+++ index.php
@@ -5,9 +5,12 @@
     </head>
     <body>
         <header><h1>CST-338 Work</h1></header>
+     <h2>Homework</h2>
      <a href="homework/homework_1/index.php">Homework 1: Arduino Tutorial</a> <br/>
      <a href="homework/homework_2/index.php">Homework 2: Dice Roller</a> <br/>
-     <a href="labs/lab2/index.php">Lab 2 Slot Machine</a> <br/>
+     <h2>Labs</h2>
      <a href="labs/lab1/index.php">Lab 1 Personal Portfolio</a> <br/>
+     <a href="labs/lab2/index.php">Lab 2 Slot Machine</a> <br/>
+     <a href="labs/lab3/index.php">Lab 3 SilverJack Card Game</a> <br/>
     </body>
 </html>
