01cd237bb343ab8c40a822f72cfc814f886efc24
Jeremy Locatelli <jlocatelli@csumb.edu>
2017-09-29 20:32:47 +0000

    added caurosel framework to lab 4 need to finish images arnt showing up on the caurosel but at least it works whatever

--- labs/lab4/index.php
+++ labs/lab4/index.php
@@ -21,21 +21,64 @@ if (isset($_GET['keyword'])) {
     </head>
     <body>
         <br/> <br/>
+         <form>
+            <input type="text" name="keyword" placeholder="Keyword" />
+            <input type="submit" value="Submit" />
+        </form>
         
         <?php
             if(!isset($imageURLs)) {
                 echo "<h2>Type a keyword to display a slideshow <br/> with random images from Pixibay.com </h2>";
-            } else {
+            } else 
+            {
                 // display carousel
-                for ($i = 0; $i < 5; $i++) {
-                     echo "<img src='" .$imageURLs[$i]."' width='200'>";
+            ?>
+            <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                <!--Indicators Here -->
+                <ol class="carousel-indicators">
+                    <?php
+                    for ($i = 0; $i < 5; $i++) {
+                         echo "<li data-target='#carousel-exaple-generic' data-slide-to='$i'";
+                         echo ($i==0)?" class='active'": "";
+                         echo '></li>';
+                    }
+                    ?>
+                </ol>
+                <!--Wrapper for Images -->
+                <div class="carousel-inner" role="listbox">
+                    <?php 
+                    for ($i = 0; $i < 5; $i++) 
+                    {
+                        do 
+                        {
+                            $randomIndex = rand(0,count($imageURLs));
+                        }
+                     while (!isset($imageURLs[$randomIndex]));
+                     echo '<div class="item ';
+                     echo ($i==0)?"active": ""; //what the fuck is this
+                     echo '">';
+                     echo '<img src"'.$imageURLs[$randomIndex].'">';
+                     echo '</div>';
+                     unset($imageURLs[$randomIndex]);
+                    }
+            ?>
+                </div>
+                <!-- Controls Here -->
+                <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+             <?php
                 }
-            }
-        ?>
-        <form>
-            <input type="text" name="keyword" placeholder="Keyword" />
-            <input type="submit" value="Submit" />
-        </form>
+             ?>
+       
+        
+       
         <br/> <br/>
     </body>
 </html>
