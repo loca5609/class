c59b3a30ed36d9ca6bd8d8bcc0c5f87ef78ed891
Jeremy Locatelli <jlocatelli@csumb.edu>
2017-11-29 20:25:49 +0000

    All working and tidyed up HW 5

--- homework/homework_5/index.php
+++ homework/homework_5/index.php
@@ -2,21 +2,24 @@
     <head>
        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"> 
        <link rel="stylesheet" type="text/css" href="css/styles.css" />   
+       <link href="https://fonts.googleapis.com/css?family=Barlow+Semi+Condensed" rel="stylesheet"> 
        <script src="https://code.jquery.com/jquery-3.1.0.js"></script>
        
     <script>
-    var dateInput = $("#date").val();
+    
     
         function apiCall(){
+            var baseInput = $("#base").val();
             $.ajax({
 
             type: "GET",
-            url: "https://api.fixer.io/latest?base",
+            url: "https://api.fixer.io/latest?",
             dataType: "json",
             data: {"base": $("#base").val()},
             success: function(data,status) {
               //rates is the assoc. array of currency types, the key is the currency code, value is the exchange from the base currency
               //alert(data.rates["AUD"]);
+              $("#rates").append("<h3>"+baseInput+" Exchange Rates: <br></h3>");
               $("#rates").append("Australia "+data.rates["AUD"]+"<br>");
               $("#rates").append("Canada "+data.rates["CAD"]+"<br>");
               $("#rates").append("Japanese Yen "+data.rates["JPY"]+"<br>");
@@ -32,7 +35,9 @@
         });//ajax
         
     }  //function
-        
+       $(document).ready(  function(){
+        $("#button").click(function(){ apiCall(); });
+    } ); //documentReady
     </script>
     
     </head>    
@@ -42,10 +47,13 @@
         <p><h3>Date entry instructions</h3>
         This site allows you to view the currency exchange rates of a certain currency compared to 5 other currencies 
         </p>
-        <form>
+        <form onsubmit="return false">
             <input type="text" id="base"/>
             Date
-            <button onClick="apiCall()">GO</button>
+            <button id="button">GO</button>
+        </form>
+        <form>
+            <button id="clear">Clear Screen</button>
         </form>
         <div id="rates">
             
