dc21c1f4b240a64f40768ab313e4977ea5738994
Jeremy Locatelli <jlocatelli@csumb.edu>
2017-12-05 04:33:03 +0000

    Almost done with admin page need to finish up report generation

--- final/admin.php
+++ final/admin.php
@@ -1,17 +1,82 @@
 <?php
 include 'php/header.php';
+
+include '../dbConn.php';
+//Game Modal insert record
+if(isset($_GET['gameSubmit'])){
+  
+  $sql = "INSERT INTO game (name,publisher,release_date,system) VALUES (:name,:publisher,:release,:system)";
+  $namedParameters[':name'] = $_GET['gameName'];
+  $namedParameters[':publisher'] = $_GET['gamePub'];
+  $namedParameters[':release'] = $_GET['gameRelease'];
+  $namedParameters[':system'] = $_GET['gameSystem'];
+                            
+            $stmt = $conn->prepare($sql);
+            
+            $stmt->execute($namedParameters);
+}
+
+function nesReport(){
+  //generate games report
+  global $conn;
+  $sql = "SELECT count(system) as nesCount FROM `game` WHERE system LIKE '%NES%'";
+  $stmt = $conn->prepare($sql);
+  $stmt->execute();
+  $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  foreach($records as $record){
+    echo "NES Games: ". $record[nesCount] ."<br> ";
+  }
+  
+}
+
+function genReport(){
+  global $conn;
+  $sql = "SELECT count(system) as genCount FROM `game` WHERE system LIKE '%Genesis%'";
+  $stmt = $conn->prepare($sql);
+  $stmt->execute();
+  $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  foreach($records as $record){
+    echo "Genesis Games: ". $record[genCount] ."<br> ";
+  }
+}
+
+function saturnReport(){
+  global $conn;
+  $sql = "SELECT count(system) as satCount FROM `game` WHERE system LIKE '%Saturn%'";
+  $stmt = $conn->prepare($sql);
+  $stmt->execute();
+  $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  foreach($records as $record){
+    echo "Saturn Games: ". $record[satCount] ."<br> ";
+  }
+}
+  
+ 
+
+            
+            
 ?>
 
 <script>
     $(document).ready(function() {
         
-        $("#addItem").click(function(){
-            $('#addItemModal').modal("show");
+        $("#addGame").click(function(){
+            $('#addGameModel').modal("show");
             
         });//click
         
+        $("#report").click(function(){
+          $("#reportModal").modal("show");
+        });//click
+        
+       
+      
+        
+       
+        
         
     });//doc ready
+    
 </script>
 
 
@@ -22,30 +87,85 @@ include 'php/header.php';
     
     <body>
         <!-- Button trigger modal -->
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#add">
-  Launch demo modal
-</button>
+        <button type="button" id="addGame" class="btn btn-primary">
+          Add Game
+        </button> <br>
+        <button type="button" id="report" class="btn btn-primary">
+          View Reports
+        </button>
+       
         
-        <!-- Modal -->
-        <div class="modal" tabindex="-1" role="dialog" id="addItemModal">
+        <!-- Modal Add Game-->
+        <div  id="addGameModel" class="modal" tabindex="-1" role="dialog">
           <div class="modal-dialog" role="document">
             <div class="modal-content">
               <div class="modal-header">
-                <h5 class="modal-title">Modal title</h5>
+                <h5 class="modal-title">Add Game</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span>
                 </button>
               </div>
               <div class="modal-body">
-                <p>Modal body text goes here.</p>
+                <h4>Add Game to database</h4>
+                <!-- Game Form -->
+                <form method="get">
+                  Name:<br>
+                  <input type="text" name="gameName"/><br>
+                  Publisher:<br>
+                  <input type="text" name="gamePub"/><br>
+                  Release Date:<br>
+                  <input type="text" name="gameRelease"/><br>
+                  System:<br>
+                  <input type="text" name="gameSystem"/><br><br>
+                  <input type="submit" name="gameSubmit" class="btn btn-primary" text="Submit"/>
+                  </form>
+                  
+                
+                
               </div>
               <div class="modal-footer">
-                <button type="button" class="btn btn-primary">Save changes</button>
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
               </div>
             </div>
           </div>
         </div>
+        
+        
+        <div  id="reportModal" class="modal" tabindex="-1" role="dialog">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title">Add Game</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <h4>Reports</h4>
+                
+                <div id="game_report">
+                  <?=nesReport()?>
+                  <?=genReport()?>
+                  <?=saturnReport()?>
+                </div>
+                
+                <div id="system_report">
+                  
+                </div>
+                
+                <div id="accessory_report">
+                  
+                </div>
+                
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+       
                 
     </body>
     
